9beb61e8f6c0d36e49353ca31047bd372b2c6b3b
Brian-Momata <brianmomata@gmail.com>
2023-03-26 10:49:46 +0300

    Fix issue of post not saving in db

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,12 +2,11 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def new
-    
+    @post = Post.new
   end
 
   def create
-    @post = Post.new(params[:body])
-    @post.user_id = session[:current_user_id]
+    @post = current_user.posts.new(post_params)
 
     if @post.save
       redirect_to action: 'index'
@@ -31,5 +30,12 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
+    render :index
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
   end
 end
